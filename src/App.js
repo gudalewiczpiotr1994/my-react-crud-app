b38a0cb511999ccda6adb00844b1f9bf74b21344
Piotr Gudalewicz <gudalewiczpiotr@gmail.com>
2019-01-28 16:34:36 +0100

    Added: main section, footer ; Changed: header text from css animation to jquery animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import classes from './Components/Footer/Footer.css';
 import Navbar from './Components/Navbar/Navbar'
 class App extends Component {
   render() {
+    const copy = "\u00A9"
+
     return (
       <div className="App">
             <Navbar/>
+            <footer className={classes.Footer}>
+                <p className={classes.Text}>
+                    {copy} Piotr Gudalewicz
+                </p>
+            </footer>
       </div>
     );
   }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import classes from './Home.css';
+import $ from 'jquery';
+import usersImage from '../../Assets/users.png';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class Home extends Component {
 
     state= {
@@ -15,28 +18,80 @@ class Home extends Component {
             textString = res.text;
         })
 
+        let text = <p className={classes.Typewriter}>{textString}</p>
+        let textStyle = $('.Home__Typewriter__33iS_');
+
+        textStyle.animate({
+            width: '325px'
+        },2000,function(){
+            $(this).animate({
+                width: '0'
+            },2000)
+        });
+
         let changeText = setTimeout( () => {
             this.state.helloString.map( res => {
-                if(res.id=== true){
-                    this.setState({helloString: [{text : "Welcome to My React CRUD App", id:false}]})
-                    textString = res.text;
+                if(res.id === true){
+                    this.setState({helloString: [{text : "Hello. My name is Peter", id:false}]})
+
                 }
-                if(res.id==false){
-                    this.setState({helloString: [{text : "Hello. My name is Peter", id:true}]})
-                }
+                if(res.id === false){
+                    this.setState({helloString: [{text : "Welcome to My React CRUD App", id:true}]})
+                    textStyle.stop().animate({
+                        width: '325px'
+                    },2000,function(){
+                        $(this).animate({
+                            width: '0'
+                        },2000)
+                    });               
+                 }
             });
-          },4000);
+            clearTimeout(changeText);
+        },4500);
+
         
 
+        let logo = "../../Assets/Users.png";
         return ( 
-            <div className={classes.MainTheme}>
-            <div className="container text-center">
-              <h1 className={classes.Header}>My-React-CRUD-App</h1>
-              <div>
-                <p className={classes.Typewriter}>{textString}</p>
-              </div>                
+            <div className="text-center">
+                <div className={classes.MainTheme}>
+                  <h1 className={classes.Header}>My-React-CRUD-App</h1>
+                  <div>
+                      {text}
+                  </div>             
+                </div>
+                <div className="container text-center">
+                    <div className="row p-5">
+                        <div className="col-md-6">
+                            <img className={classes.userInterface} src={usersImage}></img>
+                        </div>
+                        <div className="col-md-6">
+                            <p className={classes.Information}>
+                                Welcome in my React-CRUD-App.
+                            </p>
+                            <p className={classes.Information}>
+                                I have created this application for simply purpose. This is my first apllication where i did a lot of work for React by myself.
+                            </p>
+
+                            <p className={classes.Information}>
+                               I hope i will be improving my skill all the time and explore React's possiblities every day.
+                            </p>
+
+                            
+                            <p className={classes.Information}>
+                               Please, check this out!
+                            </p>
+
+                            <Link className="nav-link" to="/users">
+                            <button className={classes.Button}>
+                                Users
+                            </button>
+                            </Link>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
          );
     }
 }
