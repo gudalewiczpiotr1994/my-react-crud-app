3f40636d2129aa069cfc6fd5c015da39fc2fb8db
Piotr Gudalewicz <gudalewiczpiotr@gmail.com>
2019-02-07 12:08:32 +0100

    Added: form sign up

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
 import classes from './SignUp.css';
 class SignUp extends Component {
-    state = {  }
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail Address'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLenght: 6
+                },
+                valid: false,
+                touched: false
+            }
+        }
+      }
     render() { 
-        return (
-            <div className="container">
-                <div className={classes.Form}>
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
 
+        const form = formElementsArray.map(formElement => (
+            <input
+                key={formElement.id}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.valid}
+                invalid={!formElement.config.valid}
+                shouldValidate={formElement.config.validation}
+                touched={formElement.config.touched}
+                changed={(event) => this.inputChangedHandler(event,formElement)}/>
+                ));
+        return (
+            <div className={classes.SignUp}>
+                <div className="container">
+                    <div className={classes.Form}>
+                    {form}
+                    <button className={"btn btn-block btn-success "}>Sign Up</button>
+                </div>
                 </div>
             </div>
           );
