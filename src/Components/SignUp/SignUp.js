3ab91fc762170c6ed8954f8ba2b169a2a8f17890
Piotr Gudalewicz <gudalewiczpiotr@gmail.com>
2019-02-14 15:10:09 +0100

    Added: registry validation and registry

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -10,7 +10,13 @@ class SignUp extends Component {
                     type: 'email',
                     placeholder: 'Mail Address'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+
+                },
+                valid: false
             },
             password: {
                 elementType: 'input',
@@ -18,16 +24,72 @@ class SignUp extends Component {
                     type: 'password',
                     placeholder: 'Password'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 14
+                    
+                },
+                valid: false
             }
         }
       }
 
-      onSubmitHandler = (email,password) => {
-        this.setState({email: value});
-        this.setState({password: value});
-        this.setState({[name]: value});
-        console.log(auth);
+
+      checkValidity(value, rules) {
+        
+        let isValid = true;
+        
+        if(rules.required){
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if(rules.isEmail){
+            const type =   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            isValid = type.test(value) && isValid;
+        }
+
+        return isValid;
+      }
+
+      inputChangedHandler = (event , controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value,this.state.controls[controlName].validation)
+            }
+        };
+        this.setState({controls: updatedControls});
+      }
+
+      onSubmitHandler = () => {
+          let updatedState = {
+              ...this.state.controls
+          }
+          const data = {
+                email: updatedState.email.value,
+                password: updatedState.password.value,
+                returnSecureToken: true 
+          }
+          
+          if(this.state.controls.password.valid === true && this.state.controls.email.valid === true ){
+            axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCAUePJP_C-MavetbBP2JTafM7dTyyjOqs', data)
+            .then(res => {
+                res = data;
+            })
+          }
+
       }
 
     render() { 
@@ -39,25 +101,30 @@ class SignUp extends Component {
             });
         }
 
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
+            <div className="form-group offset-md-3 col-md-6" key={formElement.id}>
             <input className="form-control my-3"
-                key={formElement.id}
+                value={formElement.config.value}
                 type={formElement.config.elementConfig.type}
                 placeholder={formElement.config.elementConfig.placeholder}
-                value={formElement.config.value}
-                onChange={(event) => this.onSubmitHandler(event)}
-                />
+                onChange={(event) => this.inputChangedHandler(event, formElement.id)}
+                required/>
+            </div>
                 ));
+                
         return (
             <div className={classes.SignUp}>
-            {this.onSubmitHandler()}
 
                 <div className="container text-center">
+                <h2>Sing Up</h2>
+
                     <div className={classes.Form}>
-                    <h2>Sing Up</h2>
-                    {form}
-                    <button type="submit" className={"btn " + classes.Button}>Sign Up</button>
+                    <form>
+                        {form}
+                    </form>
                 </div>
+                <button className={"btn " + classes.Button} onClick={this.onSubmitHandler} >Sign Up</button>
+
                 </div>
             </div>
           );
